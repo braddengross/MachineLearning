a58ed337a8e24d99edd98da6ed037900d2c9cb69
Brad Gross <brad.gross@bettercloud.com>
2016-01-19 09:39:13 -0600

    Formatting

--- chapter2/handwriting.py
+++ chapter2/handwriting.py
@@ -40,7 +40,7 @@ def handwritingClassTest():
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
         print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumstr))
         if(classifierResult != classNumstr): errorCount += 1.0
-    print("\nthe total number of errors is: %d" % errorCount)
-    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+    print("\nThe total number of errors is: %d" % errorCount)
+    print("\nThe total error rate is: %f" % (errorCount/float(mTest)))
 
 handwritingClassTest()
