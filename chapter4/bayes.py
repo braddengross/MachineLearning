090952dce03301f8709da015b544e3ae6fcb4631
Brad Gross <bradden.gross@gmail.com>
2016-01-23 18:13:09 -0600

    Naive bayes training

--- chapter4/bayes.py
+++ chapter4/bayes.py
@@ -1,3 +1,4 @@
+import numpy as np
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
             ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -22,4 +23,23 @@ def setOfWordsToVector(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else: print("the word: %s is not in my vocabulary!" % word)
     return returnVec
+
+def trainNB(trainingMatrix, trainingCatgories):
+    numberOfTrainingDocs = len(trainingMatrix)
+    numberOfWords = len(trainingMatrix[0])
+    pAbusive = sum(trainingCatgories) / float(numberOfTrainingDocs)
+    p0Numerator = np.zeros(numberOfWords)
+    p1Numerator = np.zeros(numberOfWords)
+    p0Denominator = 0.0
+    p1Denominator = 0.0
+    for i in range(numberOfTrainingDocs):
+        if trainingCatgories[i] == 1:
+            p1Numerator += trainingMatrix[i]
+            p1Denominator += sum(trainingMatrix[i])
+        else:
+            p0Numerator += trainingMatrix[i]
+            p0Denominator += sum(trainingMatrix[i])
+    p1Vect = p1Numerator / p1Denominator
+    p0Vect = p0Numerator / p0Denominator
+    return p0Vect, p1Vect, pAbusive
 
