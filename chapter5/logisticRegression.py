ff425ddb2faccee4564eb36cea6ec20df8611629
Brad Gross <bradden.gross@gmail.com>
2016-02-03 11:56:40 -0600

    Gradient ascent

--- chapter5/logisticRegression.py
+++ chapter5/logisticRegression.py
@@ -1,3 +1,5 @@
+from numpy import *
+
 def loadDataSet():
     dataMatrix = []
     labelMatrix = []
@@ -7,4 +9,20 @@ def loadDataSet():
         dataMatrix.append(1.0, float(lineArray[0]), float(lineArray[1]))
         labelMatrix.append(int(lineArray[2]))
     return dataMatrix, labelMatrix
+
+def sigmoid(inX):
+    return 1.0 / (1+exp(-inX))
+
+def gradientAscent(dataMatrix, classLabels):
+    dataMatrix = matrix(dataMatrix)
+    labelMatrix = matrix(classLabels).transpose()
+    m,n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = labelMatrix - h
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights
 
